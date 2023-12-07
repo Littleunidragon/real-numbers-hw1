3f46af2e9bad29142641e4e1fcd914ac7223bab5
Littleunidragon <81423410+Littleunidragon@users.noreply.github.com>
2023-12-07 14:17:42 +0200

    Update main.go

--- main.go
+++ main.go
@@ -7,36 +7,16 @@ import (
 )
 
 func base2ToBase10(num string) (float64, error) {
-	var err error
-	if strings.ContainsAny(num, "-23456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") {
-		err = fmt.Errorf("invalid binary number")
-		return 0, err
+	numbers := strings.Split(num, ".")
+	var answer float64
+	length := len([]rune(numbers[0])) - 1
+	for i := range []rune(numbers[0]) {
+		answer += math.Pow(2, float64(length-i))
 	}
-	var nSum, mSum float64
-	n := strings.IndexRune(num, '.')
-	if n != -1 {
-		exp := float64(n - 1)
-		for _, i := range num[:n] {
-			if i == 49 {
-				nSum += 1.0 * math.Pow(2, float64(exp))
-			}
-			exp--
-		}
-		for m, j := range num[n+1:] {
-			if j == 49 {
-				mSum += 1.0 * math.Pow(2, float64(-(m+1)))
-			}
-		}
-	} else {
-		exp := len(num) - 1
-		for _, i := range num {
-			if i == 49 {
-				nSum += 1.0 * math.Pow(2, float64(exp))
-			}
-			exp--
-		}
+	for i := range []rune(numbers[1]) {
+		answer += math.Pow(2, float64(-i-1))
 	}
-	return nSum + mSum, nil
+	return answer, nil
 }
 
 func base10ToBase2(num float64) (string, error) {
